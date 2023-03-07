74dcfbec2e32e0d3dd4a9a0e2c1a89e36639e4fe
George Poole <gpoole@oneirosolutions.com>
2023-03-07 15:55:18 +0000

    updates taking yaml out of github

--- keycloak.tf
+++ keycloak.tf
@@ -10,37 +10,8 @@ locals {
 }
 
 resource "kubectl_manifest" "keycloak_deployment" {
-  count   = local.keycloak.enabled ? 1 : 0
-  yaml_body = <<-YAML
-  apiVersion: k8s.keycloak.org/v2alpha1
-  kind: Keycloak
-  metadata:
-    name: keycloak
-  spec:
-    instances: 1
-    additionalOptions:
-      - name: storage
-        value: jpa
-      - name: proxy
-        value: none
-    db:
-      vendor: postgres
-      host: cockroachdb
-      database: keycloak
-      port: 26257
-      usernameSecret:
-        name: keycloak-db-secret-1
-        key: username
-      passwordSecret:
-        name: keycloak-db-secret-1
-        key: password
-    http:
-      tlsSecret: "stage.ireland.dlx.digital-tls"
-    hostname:
-      hostname: "stage.ireland.dlx.digital"
-    ingress:
-      enabled: true
-  YAML
+  for_each  = local.keycloak.enabled ? 1 : 0
+  yaml_body = local.keycloak.extra_values
 
   depends_on = [
     kubectl_manifest.keycloak-operator
