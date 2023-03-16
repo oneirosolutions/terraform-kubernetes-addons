065f906710bc09dba345c01e8946bf77a0e4fbcf
George Poole <gpoole@oneirosolutions.com>
2023-03-16 17:18:21 +0000

    revert changes

--- keycloakRealmImport.tf
+++ keycloakRealmImport.tf
@@ -3,8 +3,8 @@ locals {
   keycloakRealmImport = merge(
     {
       enabled                  = false
-      keycloak_client_secret   = ""
-      file_path                = "../../../../../../provider-config/eks-addons/keycloak/keycloakRealmImport.yaml"
+#      keycloak_client_secret   = ""
+#      file_path                = "../../../../../../provider-config/eks-addons/keycloak/keycloakRealmImport.yaml"
     },
     var.keycloakRealmImport
   )
@@ -20,9 +20,9 @@ resource "null_resource" "wait_for_pod" {
     kubectl_manifest.keycloak_deployment
   ]
 }
-data "local_file" "keycloakRealmImport_yaml" {
-  filename = local.keycloakRealmImport.file_path
-}
+#data "local_file" "keycloakRealmImport_yaml" {
+#  filename = local.keycloakRealmImport.file_path
+#}
 #data "template_file" "keycloakRealmImport_yaml" {
 #  template = data.local_file.keycloakRealmImport_yaml.content
 #  vars = {
@@ -33,7 +33,8 @@ resource "kubectl_manifest" "keycloakRealmImport_deployment" {
   count   = local.keycloakRealmImport.enabled ? 1 : 0
   force_new = true
 #  yaml_body = data.template_file.keycloakRealmImport_yaml.rendered
-  yaml_body = data.local_file.keycloakRealmImport_yaml.content
+#  yaml_body = data.local_file.keycloakRealmImport_yaml.content
+  yaml_body = local.keycloakRealmImport.extra_values
 
   depends_on = [
     kubectl_manifest.keycloak-operator,
