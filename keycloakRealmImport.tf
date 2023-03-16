026c86af2f02cbd1b5cd2b39bde8f6a7044c3c91
George Poole <gpoole@oneirosolutions.com>
2023-03-16 14:45:30 +0000

    update to realmImport

--- keycloakRealmImport.tf
+++ keycloakRealmImport.tf
@@ -3,12 +3,13 @@ locals {
   keycloakRealmImport = merge(
     {
       enabled                  = false
+      file_path                = "../../../../../../provider-config/eks-addons/keycloak/keycloakRealmImport.yaml"
+      keycloak_client_secret   = ""
     },
     var.keycloakRealmImport
   )
 
 }
-
 resource "null_resource" "wait_for_pod" {
   provisioner "local-exec" {
     command = "kubectl wait pod/keycloak-0 --for=condition=Ready"
@@ -19,11 +20,16 @@ resource "null_resource" "wait_for_pod" {
     kubectl_manifest.keycloak_deployment
   ]
 }
-
+data "template_file" "keycloakRealmImport_yaml" {
+  template = "${file(local.file_path)}"
+  vars = {
+    keycloak_client_secret = "${local.keycloak_client_secret}"
+  }
+}
 resource "kubectl_manifest" "keycloakRealmImport_deployment" {
   count   = local.keycloakRealmImport.enabled ? 1 : 0
   force_new = true
-  yaml_body = local.keycloakRealmImport.extra_values
+  yaml_body = "${data.template_file.keycloakRealmImport_yaml.rendered}"
 
   depends_on = [
     kubectl_manifest.keycloak-operator,
