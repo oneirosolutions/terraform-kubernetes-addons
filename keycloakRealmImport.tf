70db253d7e75c201cc503e93fbb3a44579b5b980
George Poole <gpoole@oneirosolutions.com>
2023-03-16 16:50:19 +0000

    bug fix

--- keycloakRealmImport.tf
+++ keycloakRealmImport.tf
@@ -4,6 +4,7 @@ locals {
     {
       enabled                  = false
       keycloak_client_secret   = ""
+      file_path                = "../../../../../../provider-config/eks-addons/keycloak/keycloakRealmImport.yaml"
     },
     var.keycloakRealmImport
   )
@@ -19,8 +20,11 @@ resource "null_resource" "wait_for_pod" {
     kubectl_manifest.keycloak_deployment
   ]
 }
+data "local_file" "keycloakRealmImport_yaml" {
+  filename = local.keycloakRealmImport.file_path
+}
 data "template_file" "keycloakRealmImport_yaml" {
-  template = local.keycloakRealmImport.extra_values
+  template = data.local_file.keycloakRealmImport_yaml.content
   vars = {
     keycloak_client_secret = local.keycloakRealmImport.keycloak_client_secret
   }
