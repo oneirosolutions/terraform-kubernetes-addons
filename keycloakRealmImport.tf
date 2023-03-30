761a965531cf4dad59b62a61185a269aa4ae10d1
George Poole <gpoole@oneirosolutions.com>
2023-03-30 16:29:04 +0100

    testing using file in local

--- keycloakRealmImport.tf
+++ keycloakRealmImport.tf
@@ -3,7 +3,10 @@ locals {
   keycloakRealmImport = merge(
     {
       enabled                     = false
-      realmImportPath             = ""
+      bitbucket_username          = ""
+      bitbucket_access_token      = ""
+      aws_secret_id               = ""
+      realmImport                 = ""
       keycloak_hostname           = ""
       keycloak_dlx_uri            = ""
       keycloak_dlx_monitoring_uri = ""
@@ -28,19 +31,31 @@ resource "null_resource" "wait_for_pod" {
 }
 resource "kubectl_manifest" "keycloakRealmImport_deployment" {
   count     = local.keycloakRealmImport.enabled ? 1 : 0
-  yaml_body = templatefile("../../../../../provider-config/eks-addons/keycloak/realmImport.yaml", {
-        keycloak_hostname           = local.keycloakRealmImport.keycloak_hostname
-        keycloak_dlx_uri            = local.keycloakRealmImport.keycloak_dlx_uri
-        keycloak_dlx_monitoring_uri = local.keycloakRealmImport.keycloak_dlx_monitoring_uri
-        keycloak_backend_secret     = local.keycloakRealmImport.keycloak_backend_secret
-        keycloak_admin_partyId      = local.keycloakRealmImport.keycloak_admin_partyId
-        keycloak_admin_password     = local.keycloakRealmImport.keycloak_admin_password
-        keycloak_loader_secret      = local.keycloakRealmImport.keycloak_loader_secret
-      })
-
-  depends_on = [
-    kubectl_manifest.keycloak-operator,
-    kubectl_manifest.keycloak_deployment,
-    null_resource.wait_for_pod  
-  ]
+  yaml_body = local.keycloakRealmImport.realmImport
 }
+//resource "aws_secretsmanager_secret_version" "my_secret" {
+//  secret_id = local.keycloakRealmImport.aws_secret_id
+//}
+//provider "bitbucket" {
+//  username     = ""
+//  app_password = ""
+//}
+
+//resource "kubectl_manifest" "keycloakRealmImport_deployment" {
+//  count     = local.keycloakRealmImport.enabled ? 1 : 0
+//  yaml_body = templatefile(local.keycloakRealmImport.realmImport, {
+//        keycloak_hostname           = local.keycloakRealmImport.keycloak_hostname
+//        keycloak_dlx_uri            = local.keycloakRealmImport.keycloak_dlx_uri
+//        keycloak_dlx_monitoring_uri = local.keycloakRealmImport.keycloak_dlx_monitoring_uri
+//        keycloak_backend_secret     = local.keycloakRealmImport.keycloak_backend_secret
+//        keycloak_admin_partyId      = local.keycloakRealmImport.keycloak_admin_partyId
+//        keycloak_admin_password     = local.keycloakRealmImport.keycloak_admin_password
+//        keycloak_loader_secret      = local.keycloakRealmImport.keycloak_loader_secret
+//      })
+
+//  depends_on = [
+//    kubectl_manifest.keycloak-operator,
+//    kubectl_manifest.keycloak_deployment,
+//    null_resource.wait_for_pod  
+//  ]
+//}
