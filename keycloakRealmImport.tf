1e19890ba1affafaf672ba45f37be1ada8f4c1b9
George Poole <gpoole@oneirosolutions.com>
2023-03-30 11:54:22 +0100

    update to file path

--- keycloakRealmImport.tf
+++ keycloakRealmImport.tf
@@ -28,7 +28,7 @@ resource "null_resource" "wait_for_pod" {
 }
 resource "kubectl_manifest" "keycloakRealmImport_deployment" {
   count     = local.keycloakRealmImport.enabled ? 1 : 0
-  yaml_body = templatefile("${path.module}/../../../../../../provider-config/eks-addons/keycloak/realmImport.yaml", {
+  yaml_body = templatefile("../../../../../provider-config/eks-addons/keycloak/realmImport.yaml", {
         keycloak_hostname           = local.keycloakRealmImport.keycloak_hostname
         keycloak_dlx_uri            = local.keycloakRealmImport.keycloak_dlx_uri
         keycloak_dlx_monitoring_uri = local.keycloakRealmImport.keycloak_dlx_monitoring_uri
