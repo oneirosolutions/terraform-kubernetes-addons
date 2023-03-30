87bed67725c9dac666c62ff6d146a2f83adf7330
George Poole <gpoole@oneirosolutions.com>
2023-03-30 11:47:34 +0100

    testing path.module comand

--- keycloakRealmImport.tf
+++ keycloakRealmImport.tf
@@ -26,25 +26,21 @@ resource "null_resource" "wait_for_pod" {
     kubectl_manifest.keycloak_deployment
   ]
 }
-resource "local_file" "myfile" {
-  content = "Example text"
-  filename = "./test.txt"
-}
-//resource "kubectl_manifest" "keycloakRealmImport_deployment" {
-//  count     = local.keycloakRealmImport.enabled ? 1 : 0
-//  yaml_body = templatefile(local.keycloakRealmImport.realmImportPath, {
-//        keycloak_hostname           = local.keycloakRealmImport.keycloak_hostname
-//        keycloak_dlx_uri            = local.keycloakRealmImport.keycloak_dlx_uri
-//        keycloak_dlx_monitoring_uri = local.keycloakRealmImport.keycloak_dlx_monitoring_uri
- //       keycloak_backend_secret     = local.keycloakRealmImport.keycloak_backend_secret
- //       keycloak_admin_partyId      = local.keycloakRealmImport.keycloak_admin_partyId
- //       keycloak_admin_password     = local.keycloakRealmImport.keycloak_admin_password
-//        keycloak_loader_secret      = local.keycloakRealmImport.keycloak_loader_secret
-//      })
+resource "kubectl_manifest" "keycloakRealmImport_deployment" {
+  count     = local.keycloakRealmImport.enabled ? 1 : 0
+  yaml_body = templatefile("${path.module}/../../../../../../provider-config/eks-addons/keycloak/realmImport.yaml", {
+        keycloak_hostname           = local.keycloakRealmImport.keycloak_hostname
+        keycloak_dlx_uri            = local.keycloakRealmImport.keycloak_dlx_uri
+        keycloak_dlx_monitoring_uri = local.keycloakRealmImport.keycloak_dlx_monitoring_uri
+        keycloak_backend_secret     = local.keycloakRealmImport.keycloak_backend_secret
+        keycloak_admin_partyId      = local.keycloakRealmImport.keycloak_admin_partyId
+        keycloak_admin_password     = local.keycloakRealmImport.keycloak_admin_password
+        keycloak_loader_secret      = local.keycloakRealmImport.keycloak_loader_secret
+      })
 
-//  depends_on = [
-//    kubectl_manifest.keycloak-operator,
-//    kubectl_manifest.keycloak_deployment,
-//    null_resource.wait_for_pod  
-//  ]
-//}
+  depends_on = [
+    kubectl_manifest.keycloak-operator,
+    kubectl_manifest.keycloak_deployment,
+    null_resource.wait_for_pod  
+  ]
+}
