e25423aa0d751016363d19150cae4c68c4ab4992
George Poole <gpoole@oneirosolutions.com>
2023-04-04 16:41:42 +0100

    removing var

--- keycloakRealmImport.tf
+++ keycloakRealmImport.tf
@@ -25,21 +25,18 @@ resource "null_resource" "wait_for_pod" {
     kubectl_manifest.keycloak_deployment
   ]
 }
-//data "local_file" "realmImport" {
-//  filename = "${file("${path.cwd}/../../../../../../../../../../../provider-config/eks-addons/keycloak/realmImport-test.yaml")}"
-//}
 resource "kubectl_manifest" "keycloakRealmImport_deployment" {
   count     = local.keycloakRealmImport.enabled ? 1 : 0
   yaml_body = templatefile(
     "${path.cwd}/../../../../../../../../../../../provider-config/eks-addons/keycloak/realmImport.yaml",
     {
-      var.keycloak_hostname = local.keycloakRealmImport.keycloak_hostname
-      var.keycloak_dlx_uri = local.keycloakRealmImport.keycloak_dlx_uri
-      var.keycloak_dlx_monitoring_uri = local.keycloakRealmImport.keycloak_dlx_monitoring_uri
-      var.keycloak_backend_secret = local.keycloakRealmImport.keycloak_backend_secret
-      var.keycloak_admin_partyId = local.keycloakRealmImport.keycloak_admin_partyId
-      var.keycloak_admin_password = local.keycloakRealmImport.keycloak_admin_password
-      var.keycloak_loader_secret = local.keycloakRealmImport.keycloak_loader_secret
+      keycloak_hostname = local.keycloakRealmImport.keycloak_hostname
+      keycloak_dlx_uri = local.keycloakRealmImport.keycloak_dlx_uri
+      keycloak_dlx_monitoring_uri = local.keycloakRealmImport.keycloak_dlx_monitoring_uri
+      keycloak_backend_secret = local.keycloakRealmImport.keycloak_backend_secret
+      keycloak_admin_partyId = local.keycloakRealmImport.keycloak_admin_partyId
+      keycloak_admin_password = local.keycloakRealmImport.keycloak_admin_password
+      keycloak_loader_secret = local.keycloakRealmImport.keycloak_loader_secret
     }
   )
 }
