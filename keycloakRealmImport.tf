a0e6fde2633d5469f7af730a820a2ac545758502
George Poole <gpoole@oneirosolutions.com>
2023-04-04 17:15:47 +0100

    adding depends on

--- keycloakRealmImport.tf
+++ keycloakRealmImport.tf
@@ -46,6 +46,7 @@ resource "kubectl_manifest" "keycloakRealmImport_deployment" {
   depends_on = [
     kubectl_manifest.keycloak-operator,
     kubectl_manifest.keycloak_deployment,
-    null_resource.wait_for_pod
+    null_resource.wait_for_pod,
+    aws_secretsmanager_secret_version.backend
   ]
 }
