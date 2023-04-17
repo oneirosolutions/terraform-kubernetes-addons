147765777a4f611f4395a3a8e612b5e54b6d75f6
George Poole <gpoole@oneirosolutions.com>
2023-04-17 13:33:26 +0100

    added array for non secret variables

--- keycloakRealmImport.tf
+++ keycloakRealmImport.tf
@@ -3,10 +3,10 @@ locals {
   keycloakRealmImport = merge(
     {
       enabled                      = false
-      keycloak_realm_name          = ""
-      keycloak_dlx_uri             = ""
-      keycloak_dlx_backend_uri     = ""
-      keycloak_admin_partyId       = ""
+      keycloak_realm_name          = [""]
+      keycloak_dlx_uri             = [""]
+      keycloak_dlx_backend_uri     = [""]
+      keycloak_admin_partyId       = [""]
       keycloak_admin_password      = ""
       keycloak_smtp_user           = ""
       keycloak_smtp_password       = ""
@@ -29,14 +29,14 @@ data "aws_secretsmanager_secret_version" "loader" {
   secret_id = local.keycloakRealmImport.keycloak_loader_secret_name
 }
 resource "kubectl_manifest" "keycloakRealmImport_deployment" {
-  count     = local.keycloakRealmImport.enabled ? 1 : 0
+  count     = local.keycloakRealmImport.enabled ? length(local.keycloakRealmImport.keycloak_realm_name) : 0
   yaml_body = templatefile(
     "${path.cwd}/../../../../../../../../../../../provider-config/eks-addons/keycloak/realmImport.yaml",
     {
-      keycloak_realm_name         = local.keycloakRealmImport.keycloak_realm_name
-      keycloak_dlx_uri            = local.keycloakRealmImport.keycloak_dlx_uri
-      keycloak_dlx_backend_uri    = local.keycloakRealmImport.keycloak_dlx_backend_uri
-      keycloak_admin_partyId      = local.keycloakRealmImport.keycloak_admin_partyId
+      keycloak_realm_name         = local.keycloakRealmImport.keycloak_realm_name[count.index]
+      keycloak_dlx_uri            = local.keycloakRealmImport.keycloak_dlx_uri[count.index]
+      keycloak_dlx_backend_uri    = local.keycloakRealmImport.keycloak_dlx_backend_uri[count.index]
+      keycloak_admin_partyId      = local.keycloakRealmImport.keycloak_admin_partyId[count.index]
       keycloak_admin_password     = local.keycloakRealmImport.keycloak_admin_password
       keycloak_smtp_user          = local.keycloakRealmImport.keycloak_smtp_user
       keycloak_smtp_password      = local.keycloakRealmImport.keycloak_smtp_password
