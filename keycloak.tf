1d5a9a74ed8f4004e40a9711ba62d5a4b8e58de2
George Poole <gpoole@oneirosolutions.com>
2023-04-20 16:14:34 +0100

    updates to keycloak tf

--- keycloak.tf
+++ keycloak.tf
@@ -3,16 +3,21 @@ locals {
   keycloak = merge(
     {
       enabled                  = false
+      keycloak_hostname        = ""
     },
     var.keycloak
   )
 
 }
-
 resource "kubectl_manifest" "keycloak_deployment" {
-  count  = local.keycloak.enabled ? 1 : 0
-  yaml_body = local.keycloak.extra_values
-
+  count     = local.keycloak.enabled ? 1 : 0
+  yaml_body = templatefile(
+    "${path.cwd}/../../../../../../../../../../../provider-config/eks-addons/keycloak/keycloak.yaml",
+    {
+      keycloak_hostname = local.keycloak.keycloak_hostname
+    }
+  )
+  force_new = true
   depends_on = [
     kubectl_manifest.keycloak-operator
   ]
