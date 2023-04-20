15d79d3c432f6f74e4ec563372f08fe11634d82f
George Poole <gpoole@oneirosolutions.com>
2023-04-20 16:33:21 +0100

    added ingress to terraform

--- keycloak.tf
+++ keycloak.tf
@@ -17,8 +17,20 @@ resource "kubectl_manifest" "keycloak_deployment" {
       keycloak_hostname = local.keycloak.keycloak_hostname
     }
   )
-  force_new = true
   depends_on = [
     kubectl_manifest.keycloak-operator
+  ]
+}
+resource "kubectl_manifest" "keycloak_ingress" {
+  count     = local.keycloak.enabled ? 1 : 0
+  yaml_body = templatefile(
+    "${path.cwd}/../../../../../../../../../../../provider-config/eks-addons/keycloak/keycloak-ingress.yaml",
+    {
+      keycloak_hostname = local.keycloak.keycloak_hostname
+    }
+  )
+  depends_on = [
+    kubectl_manifest.keycloak-operator,
+    kubectl_manifest.keycloak_deployment
   ]
 }
