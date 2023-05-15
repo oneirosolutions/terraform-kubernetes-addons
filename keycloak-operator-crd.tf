de052e226b1c4a8874e86c04acca9a6af8ffe4ea
George Poole <gpoole@oneirosolutions.com>
2023-05-15 15:43:24 +0100

    update to keycloak operator for namespaces

--- keycloak-operator-crd.tf
+++ keycloak-operator-crd.tf
@@ -5,35 +5,47 @@ locals {
       enabled = false
       version = "21.0.1"
       crd_version = "21.0.1"
+      namespace = ["default"]
     },
     var.keycloak-operator
   )
 
-  keycloak-operator_yaml_files = [
+  keycloak-operator-crd_yaml_files = [
     "https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${local.keycloak-operator.crd_version}/kubernetes/keycloaks.k8s.keycloak.org-v1.yml",
-    "https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${local.keycloak-operator.crd_version}/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml",
-    "https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${local.keycloak-operator.version}/kubernetes/kubernetes.yml"
+    "https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${local.keycloak-operator.crd_version}/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml",  
   ]
 
-  keycloak-operator_apply = local.keycloak-operator["enabled"] ? [for v in data.kubectl_file_documents.keycloak-operator[0].documents : {
-    data : yamldecode(v)
-    content : v
-    }
-  ] : null
+  keycloak-operator_yaml = "https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${local.keycloak-operator.version}/kubernetes/kubernetes.yml"
+
+#  keycloak-operator_apply = local.keycloak-operator["enabled"] ? [for v in data.kubectl_file_documents.keycloak-operator[0].documents : {
+#    data : yamldecode(v)
+#    content : v
+#    }
+#  ] : null
 
 }
-
-data "http" "keycloak-operator" {
-  for_each = local.keycloak-operator.enabled ? toset(local.keycloak-operator_yaml_files) : []
+data "http" "keycloak-operator-crd" {
+  for_each = local.keycloak-operator.enabled ? toset(local.keycloak-operator-crd_yaml_files) : []
   url      = each.key
 }
-
-data "kubectl_file_documents" "keycloak-operator" {
-  count   = local.keycloak-operator.enabled ? 1 : 0
-  content = join("\n---\n", [for k, v in data.http.keycloak-operator : v.body])
-}
-
-resource "kubectl_manifest" "keycloak-operator" {
-  for_each  = local.keycloak-operator.enabled ? { for v in local.keycloak-operator_apply : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content } : {}
+resource "kubectl_manifest" "keycloak-operator-crd" {
+  for_each  = local.keycloak-operator.enabled ? data.http.keycloak-operator-crd : []
   yaml_body = each.value
 }
+data "http" "keycloak-operator" {
+  count = local.keycloak-operator.enabled ? 1 : 0
+  url   = local.keycloak-operator_yaml
+}
+resource "kubectl_manifest" "keycloak-operator" {
+  count = local.keycloak-operator.enabled ? length(local.keycloak-operator.namespace) : 0
+  yaml_body = data.http.keycloak-operator
+  override_namespace = local.keycloak-operator.namespace[count.index]
+}
+#data "kubectl_file_documents" "keycloak-operator" {
+#  count   = local.keycloak-operator.enabled ? 1 : 0
+#  content = join("\n---\n", [for k, v in data.http.keycloak-operator : v.body])
+#}
+#resource "kubectl_manifest" "keycloak-operator" {
+#  for_each  = local.keycloak-operator.enabled ? { for v in local.keycloak-operator_apply : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content } : {}
+#  yaml_body = each.value
+#}
