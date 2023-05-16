f911df32585aa453d2ecef9b3867bea758e57f52
George Poole <gpoole@oneirosolutions.com>
2023-05-16 14:31:16 +0100

    test

--- keycloak-operator-crd.tf
+++ keycloak-operator-crd.tf
@@ -38,7 +38,7 @@ data "http" "keycloak-operator" {
 }
 data "kubectl_file_documents" "keycloak-operator" {
   count = local.keycloak-operator.enabled ? 1 : 0
-  content = data.http.keycloak-operator.body[0]
+  content = data.http.keycloak-operator[0].body
 }
 resource "kubectl_manifest" "keycloak-operator" {
   for_each = local.keycloak-operator.enabled ? toset(local.keycloak-operator.namespace) : []
